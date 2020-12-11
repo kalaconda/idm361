1834dee57df8f05af7085ef78455222c9043e527
mikaylalisiy <mol25@drexel.edu>
2020-12-10 21:54:18 -0500

    add any nessecary changes

--- hwk05/main.js
+++ hwk05/main.js
@@ -23,3 +23,67 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+function readData() {
+  // test
+  console.log('reading data');
+
+    //checking off a todo
+    $(document).ready(function() {
+        $("ul").on("click", "li", function() {
+          $(this).toggleClass("completed");
+      });
+  
+  //local storage
+
+  //checking off todo - local storage
+  var activeElement =  $(this).text();
+  console.log(activeElement);
+  localStorage.setItem("completed", activeElement);
+  });
+
+  $(document).ready(function(){
+      $("ul li").each(function( index ) {
+          if($(this).text() == localStorage.getItem("completed")){
+            $(this).addClass("completed");
+          }
+      });
+  });
+  
+  // saving text from input and list items - local storage
+    $(document).ready(function() {
+      var $button = $('.add-item');
+      var $input = $('.input');
+      var $list = $('.list');
+  
+  // get items from local storage
+    if (localStorage.getItem('user-input')) {
+    // select where the storage will be
+    $list.html(localStorage.getItem('user-input'));
+    }
+  
+  // add item 
+    $button.on('click', function() {
+  
+    $list.append('<li>' + $input.val() + '<span class ="close"> x</span></li>');
+  
+    // set to html inside the list
+    localStorage.setItem('user-input', $list.html());
+
+    // log added task items to console
+    console.log($('input').val())
+  
+    // return array of value (task items)
+    $input.val('');
+  
+    });
+  
+    // remove item
+    $('.list').on('click', '.close', function() {
+    $(this).parent().remove();
+    // save changes to local storage
+    localStorage.setItem('user-input', $list.html());
+    });
+  
+    });
+}
